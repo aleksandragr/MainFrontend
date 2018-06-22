5a15ab10f99c5084744dd8ce1fd188d582038c45
jurke95 <markivicius@hotmail.com>
2018-06-22 17:47:46 +0200

    fic

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ReguserService } from '../../services/reguser/reguser.service';
 import { Router } from '@angular/router';
+import {LogService} from '../../services/log/log.service';
+import {Reguser} from '../../reguser';
+
 
 @Component({
   selector: 'app-login',
@@ -12,7 +15,7 @@ export class LoginComponent implements OnInit {
   user1: any={};
   data: any={};
 
-  constructor(private userService: ReguserService, private router: Router) { }
+  constructor(private userService: ReguserService, private router: Router,private _logService:LogService) { }
 
   ngOnInit() {
   }
@@ -23,6 +26,8 @@ export class LoginComponent implements OnInit {
     .subscribe(data => {this.data=data;
 
       if(data.message=="User is logged in"){
+        
+        this._logService.setLocalStore(this.user1.email);
         this.router.navigate(['/firstpage']);
       }
 
--- src/app/reguser.ts
+++ src/app/reguser.ts
@@ -1,12 +1,11 @@
-<<<<<<< HEAD
-export class Reguser{
-=======
 import { Role } from "./role";
 
 export class Reguser{
 
+
+
+
     id: number;
->>>>>>> 38eb7c7ab6c92c0d1bb050ff1e5f59598bb09bed
     name: string;
     surname: string;
     email: string;
@@ -14,13 +13,10 @@ export class Reguser{
     address: string;
     active: boolean;
     blocked: boolean;
-<<<<<<< HEAD
-}
-=======
+
     confirmationtoken: string;
     role: Role;
     password1: string;
     password2: string;
 
 }
->>>>>>> 38eb7c7ab6c92c0d1bb050ff1e5f59598bb09bed
